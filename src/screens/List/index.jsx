07fff66cba82819deb81f0c144c049af09155493
Leonardo Pereira <leopereigon@gmail.com>
2023-04-06 02:20:35 -0300

    list screen

---
+++ src/screens/List/index.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header'
+import { } from  './styles'
+
+export function List() {
+
+  return (
+    <div className="App ">
+      <Header/>
+      <div className='containerlist'>
+      <h1>Pokédex</h1>
+     
+      </div>
+    </div>
+  )
+}
+
+
