652ad212e838cf39ed838872a324e915f28b7d87
Leonardo Pereira <leopereigon@gmail.com>
2023-04-06 02:20:16 -0300

    header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  background: ${(props) => props.theme['blue']};
+  padding: 1rem ;
+`
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
